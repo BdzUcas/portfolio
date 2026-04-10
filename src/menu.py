0cb7c2ee2d72b7c00ca928d264c1a8bd14d479dd
BdzUcas <bracken.zabriskie@ucas-edu.net>
2026-04-10 14:27:13 -0700

    Menus

---
+++ src/menu.py
@@ -0,0 +1,53 @@
+from tkinter import *
+#result class, used for passing information locally to globally
+class Result():
+    def __init__(self):
+        self.result = ''
+
+#menu function, for creating a menu with the given options
+def menu(options,prompt,title_text='Menu',width=0,height=0,titlesize=20,font='Helvetica',buttonsize=10):
+    #finds the longest bit of text out of the title and all the buttons
+    longest = len(prompt)*titlesize*0.6
+    for i in options:
+        length = len(i)*buttonsize*0.6
+        if length > longest:
+            longest = length
+    #if height is 0 (unset)
+    if height == 0:
+        #calculate height by amount of buttons and size of title and buttons
+        height = int(titlesize*4 + len(options) * buttonsize * 5)
+    #if width is 0 (unset)
+    if width == 0:
+        #calculate width based on longest text
+        width = int(longest) + 100
+    #create a screen
+    root = Tk()
+    root.title(title_text)
+    root.geometry(f'{width}x{height}')
+    #create a heading with given size and font
+    heading = Label(root,text=prompt,font=(font,titlesize))
+    heading.pack(pady=titlesize*1.5)
+    #create a Result object for storing what the user clicks on
+    result = Result()
+    #function that runs when a button is pushed
+    def button_push(result,text):
+        #set the result property of the given Result object to the text of the button
+        result.result = text
+        #kill the screen
+        root.destroy()
+    #make an empty list for the buttons
+    buttons = []
+    #loop through options
+    for option in options:
+        #make a button with given font and size that runs the button push function
+        button = Button(root,text=option,command = lambda option=option: button_push(result,option),font=(font,buttonsize))
+        #add it to the buttons list
+        buttons.append(button)
+        #put it on the screen
+        button.pack(pady=buttonsize/2)
+    #run the screen
+    root.mainloop()
+    #return the result property of the Result object
+    return result.result
+
+print(menu(['Option One','Option Two','Option Three'],'This is a menu'))
